70f74fceb2c12c5a4ad5b5ed4fae450807a4c8f8
joshsarna <joshsarna@gmail.com>
2018-10-16 11:37:34 -0700

    make create, update, and destroy actions for tasks

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -8,4 +8,30 @@ class Api::TasksController < ApplicationController
     @task = Task.find(params[:id])
     render 'show.json.jbuilder'
   end
+
+  def create
+    @task = Task.new(
+      name: params[:name],
+      due_date: params[:due_date],
+      completed_status: params[:completed_status]
+    )
+    @task.save
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(
+      name: params[:name] || @task.name,
+      due_date: params[:due_date] || @task.due_date,
+      completed_status: params[:completed_status] || @task.completed_status
+    )
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    render json: { message: "You deleted this task" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     get '/tasks' => 'tasks#index'
+    post '/tasks' => 'tasks#create'
     get '/tasks/:id' => 'tasks#show'
+    patch '/tasks/:id' => 'tasks#update'
+    delete '/tasks/:id' => 'tasks#destroy'
   end
 end
