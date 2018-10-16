f95cfd10c25ade74fbe2b2c8942e3aa8a7935b8c
joshsarna <joshsarna@gmail.com>
2018-10-15 22:56:19 -0700

    make index and show actions for tasks

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,2 +1,11 @@
 class Api::TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    render 'index.json.jbuilder'
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    render 'show.json.jbuilder'
+  end
 end
---
+++ app/views/api/tasks/_task.json.jbuilder
@@ -0,0 +1,6 @@
+json.id task.id
+json.name task.name
+json.due_date task.due_date
+json.completed_status task.completed_status
+json.created_at task.created_at
+json.updated_at task.updated_at
